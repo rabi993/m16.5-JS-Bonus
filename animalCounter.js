4f20b8eba7942b008e3528a9142acc30e577d302
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-19 11:24:13 +0600

    animal count like as brick calculation

--- animalCounter.js
+++ animalCounter.js
@@ -8,9 +8,28 @@
 // function  if else
 
 
-function animalCount(){
-    return animalCount;
-}
-var count = animalCount();
-console.log(count);
+// function animalCount(){
+//     return animalCount;
+// }
+// var count = animalCount();
+// console.log(count);
+
+// like as brick count
+function brickCalculator(brkPerFeet,kotoTola){
+    if(kotoTola < 11){
+        totalBrick =kotoTola*15*brkPerFeet;
+    }
+    if(kotoTola < 21){
+        totalBrick =(((kotoTola-10)*12*brkPerFeet)+(10*15*brkPerFeet));
+    }
+    else{
+        totalBrick =(((kotoTola-20)*10*brkPerFeet)+(10*12*brkPerFeet)+(10*15*brkPerFeet));
+    }
+    return totalBrick;
+    }
+    var brickPerFeet=1000;
+    var kotoTolaBuilding = 21;
+    var result = brickCalculator(brickPerFeet , kotoTolaBuilding);
+    
+    console.log("bricks-per-feet =",brickPerFeet,"  Total bricks = ",result," piece");
 
