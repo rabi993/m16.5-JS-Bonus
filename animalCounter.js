7a21ae427d4925fbfa88412d5025f33e1cc2bc1a
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-19 11:20:52 +0600

    animal count done

--- animalCounter.js
+++ animalCounter.js
@@ -1,4 +1,16 @@
-10 mile 50 per mile
-next 10 mile 100 per mile
-next  1000 per mile
+// 10 mile 50 per mile
+// next 10 mile 100 per mile
+// next  1000 per mile
+
+
+// if else
+// orrrr
+// function  if else
+
+
+function animalCount(){
+    return animalCount;
+}
+var count = animalCount();
+console.log(count);
 
