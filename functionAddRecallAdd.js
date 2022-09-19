7421899c9bf80fb7e7d7b495d1c515ff434bdd4c
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-19 11:36:53 +0600

    function call recall

--- functionAddRecallAdd.js
+++ functionAddRecallAdd.js
@@ -1,1 +1,34 @@
 
+
+function doubleIt(num){
+    var result =num*2;
+    return result;
+
+}
+// var first = doubleIt(5);
+// var second = doubleIt(26);
+// console.log(first, second);
+
+var first = doubleIt(5);
+var second = doubleIt(26);
+total =first+ second;
+console.log(total);
+
+// gkrughdrkguhdrkugh
+
+console.log('function double parameter/multiple parameter');
+function add(num1,num2){
+    var result = num1+ num2;
+    return result;
+}
+var sum = add(15,23);
+console.log(sum);
+
+// drgidruhgdiruufhdriuur
+
+function adds(num1,num2,num5){
+    var result = num1+ num2-num5;
+    return result;
+}
+var sum1 = adds(15,23,45);
+console.log(sum1);
